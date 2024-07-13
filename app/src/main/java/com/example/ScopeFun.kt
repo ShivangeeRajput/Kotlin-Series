3811e09be97cdc0c6522827be9af9a46bad33070
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-07-13 22:22:35 +0530

    more kotlin fun

--- app/src/main/java/com/example/ScopeFun.kt
+++ app/src/main/java/com/example/ScopeFun.kt
@@ -1,19 +1,28 @@
 package com.example
 
 fun main() {
-    val emp : Employee ?=null
-    emp?.name="John"
-    emp?.designation="Manager"
+    val emp = Employee()
+    emp.name="John"
+    emp.designation="Manager"
     //other than writing we can use apply function
-    emp?.apply {
+    emp.apply {
         name="Sean"
         designation="CEO"
     }
     println(emp)
     //other than writing println(emp.name),println(emp.designation)) use let function
+    //val emp :? Employee=null (in this case also you can use apply function)
     emp?.let {
         println(it.name)
         println(it.designation)
+    }
+    with(emp){
+        name="Ryan"
+        designation="CTO"
+    }
+    emp.run {
+        name="Alex"
+        designation="CFO"
     }
 }
 data class Employee(var name:String="",var designation:String="Developer"){
