b120d27e18478a4c8c6ea3621efee8cfc67483d0
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-07-16 07:12:00 +0530

    more kotlin fun

--- app/src/main/java/com/example/generics.kt
+++ app/src/main/java/com/example/generics.kt
@@ -1,16 +1,25 @@
 package com.example
 
 fun main() {
-    val iContainer=Container<Int>(30)
-    iContainer.setValue(3)
+   val obj=intContainer(3)
+    println(obj.getValue())
+    val iContainer=Container<Int>(5)
     iContainer.getValue()
-    val sContainer=Container<String>("Hello World")
+    val sContainer=Container<String>("Hello")
     sContainer.getValue()
 
 }
-//if we are creating a container for like integer and in future we have to create same for double or string so we
-//dont need to create the container again and again we can simple create a generics class
-//generic class
+
+class intContainer(var data:Int){
+    fun setValue(value: Int) {
+        data= value
+    }
+    fun getValue():Int {
+        return data
+    }
+}
+//instead of creating the same for all data types just create a generic class with <t> as type ner again and again we can simple create a generics class
+//generic class are independent of any type of data
 class Container<T>(var data:T) {
     fun setValue(value: T) {
         data= value
