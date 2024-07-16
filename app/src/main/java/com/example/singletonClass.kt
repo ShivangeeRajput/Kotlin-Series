71b0151206f17f7ef40a30b8a7daf27b499163ea
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-07-16 09:43:00 +0530

    more kotlin fun

---
+++ app/src/main/java/com/example/singletonClass.kt
@@ -0,0 +1,17 @@
+package com.example
+
+//fun main() {
+//    val obj1=singletonClass()
+//    val obj2=singletonClass()
+//    println(obj1.toString())
+//    println(obj2.toString())
+//}
+//class singletonClass {
+//}
+ //instead of this we will create the singleton object class
+
+fun main() {
+    println(singleton.toString())
+    println(singleton.toString())
+}
+object singleton{}
