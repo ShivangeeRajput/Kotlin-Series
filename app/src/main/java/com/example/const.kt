3c910c1df5dbbf521e5d48fd56281bef98aebcba
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-04 18:34:09 +0530

    updating classes

--- app/src/main/java/com/example/const.kt
+++ app/src/main/java/com/example/const.kt
@@ -6,3 +6,7 @@ object Constants{
 fun testValWithConstant(){
     val name=Constants.NAME
 }
+
+//if we don't use the const var then we will get the instance of the name
+// that is not the best practice and if we do use the const then the value will be inclined
+// not the variable name
