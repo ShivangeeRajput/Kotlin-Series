ea660a3da7e50e976001a96556169548ad743785
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-04 20:44:41 +0530

    updating classes

--- app/src/main/java/com/example/DataClasses.kt
+++ app/src/main/java/com/example/DataClasses.kt
@@ -1,16 +1,17 @@
 package com.example
 
-//fun main() {
-//    val person1= Person("Max","Silicon Valley","M")
-//    val person2= Person("Max","Silicon Valley","M")
-//
-//    val p3=person1.copy("Sam")
-//    println(p3)
-//    println(person1)//tostring
-//    println(person2)
-//    println(person1.hashCode())
-//    println(person1 == person2)//p1 equals p2
-//}
-//data class Person(var name:String, val Address: Int, val Sex:String){
-//
-//}
+fun main() {
+    val person1= Personn("Max","Silicon Valley","M")
+    val person2= Personn("Rhys","Las Vegas","M")
+
+    val p3=person1.copy("Sam")//copy
+    println(p3)
+    println(person1)//tostring
+    println(person2)
+    println(person1.hashCode())
+    println(person1 == person2)//p1 equals p2
+}
+data class Personn(var name:String, val Address: String, val Sex:String)
+
+
+
