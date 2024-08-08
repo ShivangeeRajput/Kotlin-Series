82a68227b44ce887ac012fa05eac845ad3ddee87
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-08 09:29:45 +0530

    updating classes

---
+++ app/src/main/java/com/example/crossinline.kt
@@ -0,0 +1,14 @@
+package com.example
+
+//crossinline is used to avoid non local return
+//we cant put any return inside that lambda
+inline fun teachhh(crossinline abcd:()->Unit){
+    abcd()
+}
+fun Guidee(){
+    println("Guide start")
+    teachhh {
+        println("abcd start")
+    }
+    println("Guide end")
+}
