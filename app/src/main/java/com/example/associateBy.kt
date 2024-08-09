96465a8aff6f774c7b0f2459ec4861ae65a65f99
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-09 15:03:23 +0530

    updating classes

---
+++ app/src/main/java/com/example/associateBy.kt
@@ -0,0 +1,13 @@
+package com.example
+
+data class Contact(val name:String,val number: String)
+
+fun main() {
+    val contacts= listOf(
+        Contact("John Doe","70xxxxxx23"),
+        Contact("John Cena","89xxxxxxxxx"),
+        Contact("John Abraham","98xxxxxxx")
+    )
+    val map=contacts.associateBy({it.name},{it.number})
+    println(map)
+}
