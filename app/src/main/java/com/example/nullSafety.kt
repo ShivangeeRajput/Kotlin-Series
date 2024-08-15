14717da96504d834020892279c4572467f69670a
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-15 20:02:51 +0530

    updating classes

---
+++ app/src/main/java/com/example/nullSafety.kt
@@ -0,0 +1,9 @@
+package com.example
+
+fun main() {
+    var gender:String="Male"
+    var gender2:String?=null //String? is a nullable data type it will accept value as well as null
+    var isAdult:Boolean?=null
+    gender2?.uppercase()
+
+}
