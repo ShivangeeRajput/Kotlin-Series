cf0e70c11a66a172127cd1307caddeded6d32e32
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-18 12:07:33 +0530

    updating classes

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.example.kot.R
 
 class MainActivity : AppCompatActivity() {
+    var count:Int=0
+    lateinit var tvCount: TextView
+    lateinit var btnCount: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvCount=findViewById(R.id.tvCount)
+        setText()
+    }
+    fun increment(v:View){
+        count++
+        setText()
+    }
+    fun setText(){
+        tvCount.text=count.toString()
     }
 }
