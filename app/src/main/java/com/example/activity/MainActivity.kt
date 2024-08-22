050c58f8030ea7698d1b75544f4ecafcea60c8f2
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-22 13:26:30 +0530

    updating classes

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -1,9 +1,7 @@
-package com.example
+package com.example.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.kot.R
