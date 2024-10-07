f6ff8cb149b45062b4f40291519da5ce70db5c9b
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-07 20:36:44 +0530

    Added Classes

---
+++ app/src/main/java/com/example/program/parallelAPICalls.kt
@@ -0,0 +1,16 @@
+package com.example.program
+
+import com.example.apiService
+
+class parallelAPICalls {
+
+
+}
+
+
+//fun main() {
+//    val result1 = async { apiService.getUserDetails() }
+//    val result2 = async { apiService.getUserPosts() }
+//    val userDetails = result1.await()
+//    val userPosts = result2.await()
+//}
