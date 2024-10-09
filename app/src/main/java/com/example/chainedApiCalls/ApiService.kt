ac2bc963514f810bbab452bbad70423a0f779ea2
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-09 11:38:33 +0530

    Added Classes

--- app/src/main/java/com/example/chainedApiCalls/ApiService.kt
+++ app/src/main/java/com/example/chainedApiCalls/ApiService.kt
@@ -1,4 +1,15 @@
 package com.example.chainedApiCalls
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
+
+    @GET("user/profile")
+    suspend fun getUserProfile():UserProfile
+
+    //chaining
+    @GET("user/post")
+    suspend fun getUserPost(@Path("userId") userId:String) : List<Post>
+
 }
