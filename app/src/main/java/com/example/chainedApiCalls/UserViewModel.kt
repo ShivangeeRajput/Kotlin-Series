01b36251bbeedad3af4fcd5836de7f3acd98d0b9
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-10 13:03:59 +0530

    Added Classes

--- app/src/main/java/com/example/chainedApiCalls/UserViewModel.kt
+++ app/src/main/java/com/example/chainedApiCalls/UserViewModel.kt
@@ -1,9 +1,35 @@
 package com.example.chainedApiCalls
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class UserViewModel:ViewModel() {
-    private val userProfile= MutableLiveData<UserProfile>()
+class UserViewModel(private val apiService: ApiService):ViewModel() {
+
+    private val _userProfile= MutableLiveData<UserProfile>()
+    val userProfile:LiveData<UserProfile> get() = _userProfile
+
+    private val _userPost= MutableLiveData<Post>()
+    val userPost:LiveData<Post> get() = _userPost
+
+    fun fetchUserProfileAndPosts() {
+        viewModelScope.launch {
+            try {
+                // First API Call: Fetching User Profile
+                val profile = apiService.getUserProfile()
+                _userProfile.value = profile
+
+                // Second API Call: Fetching User Posts based on User ID from Profile
+                val posts = apiService.getUserPost(profile.id)
+                _userPost.value = Post
+
+            } catch (e: Exception) {
+
+                _error.value = "Error fetching data: ${e.message}"
+            }
+        }
+    }
 
 }
