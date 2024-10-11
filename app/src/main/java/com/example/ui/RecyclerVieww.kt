ba9fc217e5aaca187135f46cd14711d44c4fc72b
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-11 20:00:56 +0530

    Added Classes

---
+++ app/src/main/java/com/example/ui/RecyclerVieww.kt
@@ -0,0 +1,21 @@
+package com.example.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kot.R
+
+class RecyclerVieww : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recycler_vieww)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
