50657f9219f00bf424b8dd653c946268c0540a62
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-14 14:37:58 +0530

    Added Classes

--- app/src/main/java/com/example/recyclerView/rvAdapter.kt
+++ app/src/main/java/com/example/recyclerView/rvAdapter.kt
@@ -1,8 +1,16 @@
 package com.example.recyclerView
 
-class rvAdapter(private val dataList: ArrayList<rvDataClass>):RecyclerVieww.Adapter<rvAdapter.ViewHolder>() {
-    class ViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.activity.RecyclerView
+import com.example.kot.R
 
+class rvAdapter(private val dataList: ArrayList<rvDataClass>):RecyclerView.Adapter<rvAdapter.ViewHolderClass>() {
+
+    class ViewHolderClass(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val rvImage = itemView.findViewById<ImageView>(R.id.image)
+        val rvTitle = itemView.findViewById<TextView>(androidx.appcompat.R.id.title)
     }
 
 }
