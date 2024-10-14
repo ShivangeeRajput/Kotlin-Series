93510a324777533c568e21ace44028352451170b
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-14 14:47:26 +0530

    Added Classes

--- app/src/main/java/com/example/recyclerView/rvAdapter.kt
+++ app/src/main/java/com/example/recyclerView/rvAdapter.kt
@@ -1,16 +1,32 @@
 package com.example.recyclerView
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import com.example.activity.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kot.R
 
-class rvAdapter(private val dataList: ArrayList<rvDataClass>):RecyclerView.Adapter<rvAdapter.ViewHolderClass>() {
+class rvAdapter(private val dataList: ArrayList<rvDataClass>) : RecyclerView.Adapter<rvAdapter.ViewHolderClass>() {
 
-    class ViewHolderClass(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val rvImage = itemView.findViewById<ImageView>(R.id.image)
-        val rvTitle = itemView.findViewById<TextView>(androidx.appcompat.R.id.title)
+    class ViewHolderClass(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val rvImage: ImageView = itemView.findViewById(R.id.image)
+        val rvTitle: TextView = itemView.findViewById(R.id.title)
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_item, parent, false)
+        return ViewHolderClass(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+        val currentItem = dataList[position]
+        holder.rvImage.setImageResource(currentItem.image)
+        holder.rvTitle.text = currentItem.title
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
 }
