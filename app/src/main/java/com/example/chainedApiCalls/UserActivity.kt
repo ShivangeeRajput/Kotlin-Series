929a1a41d3e2158e7c5a75921f31ff95bb7e6e8e
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-16 20:07:28 +0530

    Added Classes

--- app/src/main/java/com/example/chainedApiCalls/UserActivity.kt
+++ app/src/main/java/com/example/chainedApiCalls/UserActivity.kt
@@ -1,4 +1,33 @@
 package com.example.chainedApiCalls
 
-class UserActivity {
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.kot.R
+
+
+class UserActivity : AppCompatActivity() {
+    private lateinit var viewModel: UserViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        // Observing user profile data
+        viewModel.userProfile.observe(this) { profile ->
+            // Update UI with user profile data (e.g., profile.name)
+        }
+
+        // Observing user posts data
+        viewModel.userPost.observe(this) { posts ->
+            // Update RecyclerView or other UI elements with user posts
+        }
+
+
+        // Triggering the API call chain
+        viewModel.fetchUserProfileAndPosts()
+    }
 }
