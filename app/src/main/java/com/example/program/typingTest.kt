0d6b5e37d4a7cce67aaaa1f0bb05c54ea6ef5cbc
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-24 01:11:43 +0530

    Added Classes

--- app/src/main/java/com/example/program/typingTest.kt
+++ app/src/main/java/com/example/program/typingTest.kt
@@ -3,16 +3,18 @@ package com.example.program
 
 //hackerrank SDE Intern Task
 fun getDistance(word: String): Int {
-    var totalDistance = 0
+    // Write your code here
+    val keyboard="abcdefghijklmnopqrstuvwxyz"
+    val keymap=keyboard.withIndex().associate { it.value to it.index }
+    var totalDistance=0
+    for(i in 1 until word.length){
+        val currentChar=word[i]
+        val previousChar=word[i-1]
+        totalDistance=Math.abs(currentChar-previousChar)
 
-    for (i in 1 until word.length) {
-        val currentChar = word[i]
-        val previousChar = word[i - 1]
-
-        totalDistance += Math.abs(currentChar - previousChar)
     }
-
     return totalDistance
+
 }
 
 fun main(args: Array<String>) {
