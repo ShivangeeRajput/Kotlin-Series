89f3d29b1df4f4f0b8acd47aad02b71dca8deb91
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-10-25 16:36:33 +0530

    Added Classes

---
+++ app/src/main/java/com/example/glideLibrary.kt
@@ -0,0 +1,26 @@
+package com.example
+
+class glideLibrary {
+//    Glide.with(context)
+//    .load(imageurl)
+//    .into(imageView)
+}
+
+//Retrofit example
+//class NetworkModule {
+//    @Singleton
+//    @Provides
+//    fun providesRetrofit(): Retrofit{
+//      return Retrofit.Builder()
+//          .addConverterFactory(GsonConverterFactory.create())
+//          .baseUrl(BASE_URL)
+//          .build()
+
+//interface UserAPI {
+//    @POST("/auth/signup")
+//    suspend fun signUp(@Body userRequest: UserRequest) : Response<UserResponse>
+//
+//    @POST("/auth/login")
+//    suspend fun signIn(@Body userRequest: UserRequest) : Response<UserResponse>
+//
+//}
